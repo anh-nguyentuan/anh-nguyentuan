7a73f42f607d61a16db11c9825bda829b62c237b
anhnt189 <nguyentuananhd13vt2@gmail.com>
2023-04-03 00:38:14 +0700

    aaaa

--- Game4and10/src/app.js
+++ Game4and10/src/app.js
@@ -21,14 +21,16 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  ****************************************************************************/
-var arrOperatorDisplay = [];
-var arrNumber = ["None", "1","None", "2","None", "3","None", "4","None"];
+
+var arrNumber = [Ope.None, "1", Ope.None, Ope.None, "2", Ope.None, Ope.None, Ope.None, "3", Ope.None, Ope.None, "4", Ope.None];
 
 function isNumeric(val) {
     return /^-?\d+$/.test(val);
 }
 
 var HelloWorldLayer = cc.Layer.extend({
+    arrOperatorDisplay : [],
+    arrShowCaculate : [],
     sprite:null,
     ctor:function () {
         //////////////////////////////
@@ -59,8 +61,8 @@ var HelloWorldLayer = cc.Layer.extend({
             y: size.height / 2
         });
         this.addChild(this.sprite, 0);
-
-        this.createLayoutOperator()
+        this.creatLayoutCalculate();
+        this.createLayoutOperator();
 
         return true;
     },
@@ -70,16 +72,14 @@ var HelloWorldLayer = cc.Layer.extend({
     },
 
 
-
     createLayoutOperator : function()
-    {
-        var sizeX = 0;
-        var size = cc.winSize;
- 
+    { 
           var sizeX = 0;
           var size = cc.winSize;
           for(var index = 0; index < operatorConfig.length; index++){
-              var objOperatorMatch  = new OperatorMatch();
+
+            console.log('createLayoutOperator = ' + index);
+              var objOperatorMatch  = new OperatorMatch(this);
               objOperatorMatch.operatorSign = operatorConfig[index];
               var label = new cc.LabelTTF();
               label.setFontSize(18);
@@ -91,13 +91,112 @@ var HelloWorldLayer = cc.Layer.extend({
               sizeX += 30;
               objOperatorMatch.x = (size.width / 6) + sizeX;
               objOperatorMatch.y = size.height / 3 -100;
-   
-              arrOperatorDisplay.push(objOperatorMatch);
 
-              this.addChild(objOperatorMatch);
+              this.arrOperatorDisplay.push(objOperatorMatch);
+              this.addChild(this.arrOperatorDisplay[index]);
           }
+    },
+
+
+    creatLayoutCalculate : function(){
+        var sizeX = 0;
+        var size = cc.winSize;
+        for(var index = 0; index < arrNumber.length; index++){
+            if(index == 1 || index == 4 || index == 8 || index == 11){
+
+                console.log('creatLayoutCalculate number = ' + index);
+                var objNumeberMatch = new NumeberMatch(this)
+
+                var labelNumber = new cc.LabelTTF();
+                labelNumber.setFontSize(18);
+                labelNumber.setFontName("Arial");
+                labelNumber.setString(arrNumber[index])
+                labelNumber.setColor(cc.color("#FFFF00"));
+                objNumeberMatch.addChild(labelNumber);
+
+                sizeX += 30;
+                objNumeberMatch.x = (size.width / 6) + sizeX;
+                objNumeberMatch.y = size.height / 2.5;
+          
+                this.arrShowCaculate.push(objNumeberMatch);
+                this.addChild(this.arrShowCaculate[index]);
+            }
+            else{
+
+                console.log('creatLayoutCalculate Operartor = ' + index); 
+                var objOperatorMatch  = new OperatorMatch(this);
+                objOperatorMatch.operatorSign = arrNumber[index];
+                var label = new cc.LabelTTF();
+                label.setFontSize(18);
+                label.setFontName("Arial");
+                label.setString(objOperatorMatch.operatorSign)
+                label.setColor(cc.color("#FFFF00"));
+                objOperatorMatch.addChild(label);
+
+                sizeX += 30;
+                objOperatorMatch.x = (size.width / 6) + sizeX;
+                objOperatorMatch.y = size.height / 2.5;
+
+                this.arrShowCaculate.push(objOperatorMatch);
+                this.addChild(this.arrShowCaculate[index]);
+            }
+        }
+    },
+
+
+    // tinh toan ket qua
+    calculatResult : function(expression){
+        let operands = [];
+        let operators = [];
+        let current = "";
+        for (let i = 0; i < expression.length; i++) {
+          if (expression[i] == "(") {
+            let count = 1;
+            let j = i + 1;
+            while (count > 0 && j < expression.length) {
+              if (expression[j] == "(") {
+                count++;
+              } else if (expression[j] == ")") {
+                count--;
+              }
+              j++;
+            }
+            operands.push(calculate(expression.substring(i + 1, j - 1)));
+            i = j - 1;
+          } else if (expression[i] == "+" || expression[i] == "-" || expression[i] == "*" || expression[i] == "/") {
+            operators.push(expression[i]);
+            operands.push(parseFloat(current));
+            current = "";
+          } else {
+            current += expression[i];
+          }
+        }
+        operands.push(parseFloat(current));
+      
+        // Tính toán kết quả
+        while (operators.length > 0) {
+          let operator = operators.pop();
+          let operand2 = operands.pop();
+          let operand1 = operands.pop();
+          let result;
+          if (operator == "+") {
+            result = operand1 + operand2;
+          } else if (operator == "-") {
+            result = operand1 - operand2;
+          } else if (operator == "*") {
+            result = operand1 * operand2;
+          } else if (operator == "/") {
+            result = operand1 / operand2;
+          }
+          operands.push(result);
+        }
+      
+        return operands[0];
     }
 });
+
+
+
 
 var HelloWorldScene = cc.Scene.extend({
     onEnter:function () {
--- Game4and10/src/operatorLayout.js
+++ Game4and10/src/operatorLayout.js
@@ -1,145 +1,102 @@
 var OperatorMatch = cc.Sprite.extend({
 
-ctor : function(position){
-	this._super();
-	this.handleMouseIn();
-},	
 operatorSign :  Ope.None,
 isDisable : false,
 isDragging :false,
 mouseDownPosition : cc.p(0, 0),
+controlLogicLayer : null,
 
-handleMouseIn :function(OperatorMatchCaculate){
-	// Bắt sự kiện mousedown
+ctor : function(ctrlLayer){
+	this._super();
+	this.controlLogicLayer = ctrlLayer;
+},	
+
+
+onEnter : function(){
+	this._super();
 	cc.eventManager.addListener({
-		event: cc.EventListener.MOUSE,
-		onMouseDown: function(event) {
-			console.log('a111111111111111')
-			// Kiểm tra nếu con trỏ chuột đang nằm trên đối tượng
-			if (cc.rectContainsPoint(this.getBoundingBox(), event.getLocation())) {
-				this.isDragging = true;
-				mouseDownPosition = event.getLocation();
-			}
-		}
-		}, this);
+		event : cc.EventListener.TOUCH_ONE_BY_ONE,
+		swallowTouches : true,
+		onTouchBegan : this.onTouchBegan.bind(this),
+		onTouchMoved : this.onTouchMoved.bind(this),
+		onTouchEnded : this.onTouchEnded.bind(this)
+	}, this);
 	
-		// Bắt sự kiện mousemove
-		cc.eventManager.addListener({
-		event: cc.EventListener.MOUSE,
-		onMouseMove: function(event) {
-			console.log('a2222222222222222'+ this.getPosition())
+},
+
+
+onTouchBegan : function (touch, event){
+	// Kiểm tra nếu con trỏ chuột đang nằm trên đối tượng
+	if (true) {
+		this.isDragging = true;
+		mouseDownPosition = touch.getLocation();
+		console.log('mouseDownPosition ' +  touch.getLocation())
+	}
+
+	return true;
+
+},
+
+onTouchMoved:function(touch, event) {
 			// Nếu đang kéo chuột và đối tượng được chọn
 			if (this.isDragging) {
 				// Tính toán khoảng cách di chuyển
-				var delta = cc.pSub(event.getLocation(), mouseDownPosition);
+				var delta = cc.pSub(touch.getLocation(), mouseDownPosition);
+				console.log('delta = '+ delta.x + " ," + delta.y);
 	
 				// Di chuyển đối tượng tương ứng với khoảng cách di chuyển
 				var currentPosition = this.getPosition();
 
-				console.log('000' + this.getPosition()) 
-				this.setPosition(cc.pAdd(this.currentPosition, delta));
+				console.log('000 = ' + this.getPositionX() + " ," +  + this.getPositionY()) 
+				this.setPosition(cc.pAdd(currentPosition, delta));
 	
 				// Cập nhật vị trí chuột
-				mouseDownPosition = event.getLocation();
+				mouseDownPosition = touch.getLocation();
+
 			}
-		}
-	}, this);
+
+			return true;
+},
+onTouchEnded:function(touch, event){
+	console.log('a333333333333333333333333')
+	this.isDragging = false;
+
+},
+});
+
+var NumeberMatch = cc.Sprite.extend({
+	number : 0,
+	controlLogicLayer : null,
+
+	ctor : function(ctrlLayer){
+		this._super();
+		this.controlLogicLayer= ctrlLayer;
+	},	
 	
-		// Bắt sự kiện mouseup
+	onEnter : function(){
+		this._super();
 		cc.eventManager.addListener({
-		event: cc.EventListener.MOUSE,
-		onMouseUp: function(event) {
-			console.log('a333333333333333333333333')
-			this.isDragging = false;
-		}
+			event : cc.EventListener.TOUCH_ONE_BY_ONE,
+			swallowTouches : true,
+			onTouchBegan : this.onTouchBegan.bind(this),
+			onTouchMoved : this.onTouchMoved.bind(this),
+			onTouchEnded : this.onTouchEnded.bind(this)
 		}, this);
-}
+		
+	},
+	
+	
+	onTouchBegan : function (touch, event){
+	
+	},
+	
+	onTouchMoved:function(touch, event) {
+			
+	},
+	onTouchEnded:function(touch, event){
 
+	
+	},
 
 });
 
-class NumeberMatch {
-	number
-}
-
-
-var operatorLayout = ccui.Layout.extend({
- ctor :function(){
- 	this._super();
- },
-
- visibleOperator :false,
- opertorMatch : "None",
- enableOperator : true,
- scaleWith : 1,
- scaleHeight :1,
- isDragging : false,
- mouseDownPosition : cc.p(0, 0),
-
- showDisplayOperator : function(parent){
- 	var textFile = new ccui.Text();
- 		// show operator
- 		if(this.visibleOperator){
- 			var childSize = cc.size(parent.width/this.scaleWith, parent.height/this.scaleHeight);
-			console.log('width : ' + parent.width + ' height ' + parent.height)
- 			this.setContentSize(childSize);
-
- 			textFile.setText(OperatorMatch.get(this.opertorMatch));
-			 
-			textFile.setContentSize(childSize); // đặt kích thước cho Text
-			textFile.setPosition(cc.p(0, 0)); // đặt vị trí cho Text trong Layout
- 			textFile.setColor(cc.color("#FFFF00"));
-			textFile.setFontSize(30);
-			// textFile.setContentSize(childSize);
- 			this.addChild(textFile);
- 		}
- 		else{
- 			var childSize = cc.size(0,0);
- 			this.setContentSize(childSize);
-
- 			this.addChild(textFile);
- 		}
- 	},
-
-handleMouseIn :function(){
-		// Bắt sự kiện mousedown
-		cc.eventManager.addListener({
-			event: cc.EventListener.MOUSE,
-			onMouseDown: function(event) {
-				// Kiểm tra nếu con trỏ chuột đang nằm trên đối tượng
-				if (cc.rectContainsPoint(this.getBoundingBox(), event.getLocation())) {
-					this.isDragging = true;
-					mouseDownPosition = event.getLocation();
-				}
-			}
-			}, this);
-		
-			// Bắt sự kiện mousemove
-			cc.eventManager.addListener({
-			event: cc.EventListener.MOUSE,
-			onMouseMove: function(event) {
-				// Nếu đang kéo chuột và đối tượng được chọn
-				if (this.isDragging) {
-					// Tính toán khoảng cách di chuyển
-					var delta = cc.pSub(event.getLocation(), mouseDownPosition);
-		
-					// Di chuyển đối tượng tương ứng với khoảng cách di chuyển
-					var currentPosition = this.getPosition();
-					this.setPosition(cc.pAdd(currentPosition, delta));
-		
-					// Cập nhật vị trí chuột
-					mouseDownPosition = event.getLocation();
-				}
-			}
-		}, this);
-		
-			// Bắt sự kiện mouseup
-			cc.eventManager.addListener({
-			event: cc.EventListener.MOUSE,
-			onMouseUp: function(event) {
-				this.isDragging = false;
-			}
-			}, this);
-}
-
-});
